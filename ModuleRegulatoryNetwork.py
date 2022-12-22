4924dd24c67e1a6367c1370ebd63a8205f8271c8
noord087 <ben.noordijk@wur.nl>
2022-12-22 12:00:49 +0100

    Fixed bug, added docstrings, included methods for graph cleanup

--- ModuleRegulatoryNetwork.py
+++ ModuleRegulatoryNetwork.py
@@ -2,18 +2,29 @@ from __future__ import annotations
 
 import json
 from pathlib import Path
+from typing import Callable
+
 import networkx as nx
 import pandas as pd
 from matplotlib import pyplot as plt
 
 
 class ModuleRegulatoryNetwork:
-    def __init__(self, graph: nx.Graph):
+    def __init__(self, graph: nx.DiGraph):
         self.graph = graph
+        # Standard prefixes
+        self.tf_prefix = 'TF'
+        self.module_prefix = 'MODULE'
 
     @classmethod
-    def from_lpan_edge_csv(cls, lpan_file_path: Path, p_value_cutoff: float = 0.05, top_rank: int = None):
-        """Create object from lpan edge csv"""
+    def from_lpan_edge_csv(cls, lpan_file_path: Path, top_rank: int = None) -> ModuleRegulatoryNetwork:
+        """Create object from lpan edge csv.
+
+        :param lpan_file_path: Path to csv output by LPAN.
+        E.g. aracne_network_edges.csv
+        :param top_rank: If provided, keep only the top_rank  number of
+        connections. I.e. the strongest one predicted by lpan.
+        """
         some_df = pd.read_csv(lpan_file_path)
         if top_rank:
             some_df = some_df[some_df['rank'] < top_rank]
@@ -24,15 +35,54 @@ class ModuleRegulatoryNetwork:
                                           create_using=nx.DiGraph)
         return cls(a_graph)
 
-    def plot_network(self):
+    def plot_network(self, draw_func: Callable = nx.draw_kamada_kawai, out_path: Path = None):
+        """Plot network using matplotlib. Colour mean something, but I'll document that later"""
         node_color_map = ['blue' if ('TF' in node) else 'orange' for node in
                      self.graph.nodes]
         edge_color_map = ['black' if ('transcribed_by' in atrs.get('origin'))
                           else 'brown' for _, _, atrs in self.graph.edges(data=True)]
 
-        nx.draw(self.graph, node_color=node_color_map,
-                edge_color=edge_color_map)
+        draw_func(self.graph, node_color=node_color_map,
+                  edge_color=edge_color_map)
+        if out_path:
+            plt.savefig(out_path)
+        else:
+            plt.show()
 
     def add_tf_module_mappings(self, path_to_orignal_cluster: Path) -> None:
-        original_connections = nx.read_edgelist(path_to_orignal_cluster)
+        """Include what TFs are transcribed by which cluster.
+        Add these edges with this method, input file is created from ExpressionMatrix (I think)."""
+        original_connections = nx.read_edgelist(path_to_orignal_cluster, create_using=nx.DiGraph)
+        # Invert connections, because <TF:is transcribed by:Module>
+        original_connections = original_connections.reverse()
         self.graph.add_edges_from(original_connections.edges, origin='transcribed_by')
+
+    def clean_up_network(self) -> None:
+        """Remove all non-binding TFs and unused modules."""
+        self.remove_non_binding_tfs()
+        self.remove_unused_modules()
+
+    def remove_non_binding_tfs(self) -> None:
+        """Delete transcription factors from the graph that do not bind to
+        any module. I.e. that have an out_degree of 0.
+        """
+        # Find nodes which are tf and have out degree 0
+        non_binding_tfs = []
+        for node_name, out_degree in self.graph.out_degree():
+            if self.tf_prefix in node_name and out_degree == 0:
+                non_binding_tfs.append(node_name)
+        # Remove them
+        self.graph.remove_nodes_from(non_binding_tfs)
+
+    def remove_unused_modules(self) -> None:
+        """Delete modules which are unused. I.e. modules that are not regulated
+         by any transcription factor, and also don't encode for any transcription factors"""
+        unused_modules = []
+        for node_name in self.graph.nodes:
+            if (self.module_prefix in node_name
+                    and self.graph.in_degree(node_name)
+                    == self.graph.out_degree(node_name)
+                    == 0):
+                unused_modules.append(node_name)
+        # Remove them
+        self.graph.remove_nodes_from(unused_modules)
--- tests/test_ModuleRegulatoryNetwork.py
+++ tests/test_ModuleRegulatoryNetwork.py
@@ -7,12 +7,10 @@ from ModuleRegulatoryNetwork import ModuleRegulatoryNetwork
 
 
 def test_import_network_edges_from_lpan():
-    some_p_val_cutoff = 0.05
     path_to_network_edges = Path('../data/aracne_network_edges.csv')
     path_to_orignal_cluster = Path('../data/my_clustering_edgelist.csv')
     my_graph = ModuleRegulatoryNetwork.from_lpan_edge_csv(path_to_network_edges,
-                                                          top_rank=10)
+                                                          top_rank=30)
     my_graph.add_tf_module_mappings(path_to_orignal_cluster)
+    my_graph.clean_up_network()
     my_graph.plot_network()
-    # TODO you need to debug this because the colours don't always make sense.
-    plt.show()
