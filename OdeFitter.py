1382e84deffbb6290adc32933ab858300583bf65
noord087 <ben.noordijk@wur.nl>
2023-01-05 16:37:07 +0100

    Added test to investigate identifiability of network

--- OdeFitter.py
+++ OdeFitter.py
@@ -4,6 +4,7 @@ import numpy as np
 from lmfit import Parameters, minimize
 from lmfit.minimizer import MinimizerResult
 from scipy.integrate import solve_ivp
+from scipy.integrate._ivp.ivp import OdeResult
 
 from OdeInference import OdeInference
 
@@ -15,26 +16,39 @@ class OdeFitter:
         self.measured_data = measured_data
         self.time_points = time_points
         self.params = Parameters()
+        # TODO get a bug with naming the d0,1,2,3 parameters
         # TODO how to handle guesses for initial params/constraints?
+        # TODO Note that model seems really sensitive to this guess of param_limit
+        param_limit = .1
         for param_name in ode_inference.get_param_names():
             # Decay rates cannot be negative
-            min_value = -1e10 if 'd' not in param_name else 0
-            self.params.add(param_name, min=min_value, max=1e10)
+            min_value = 0. if 'd' in param_name else -param_limit
+            # Min value always at 0 helps speed up solving IVP for some reason?
+            self.params.add(param_name, min=min_value, max=param_limit)
         self.init_condition_names = []
         for i, init_value in enumerate(measured_data[:, 0]):
             init_y_name = f'y{i}'
             self.params.add(init_y_name, value=init_value, vary=False)
             self.init_condition_names.append(init_y_name)
+        # self.fitting_complete = False
 
-    def set_param_limits(self):
-        raise NotImplementedError
 
     def loss_function(
             self, params: Parameters, t: np.ndarray, y_real: np.ndarray,
             my_func: callable, t_start=None, t_end=None) -> np.ndarray:
         """Return squared residuals between y_real and a prediction of my_func
-        at time points t for a given set params.
-        """
+                at time points t for a given set params.
+                """
+        if t_start is not None or t_end is not None:
+            selected_time_points = (t_start <= t) & (t <= t_end)
+            y_real = y_real[:, selected_time_points]
+        y_pred = self.predict_values(params, t, my_func, t_start, t_end)
+        return np.square(y_pred.y - y_real)
+
+    def predict_values(self, params: Parameters, t: np.ndarray,
+                       my_func: callable, t_start=None, t_end=None) -> OdeResult:
+        """Return values at time points t, given a set of params"""
+        # TODO make this an OdeInference method, instead of it belonging to this class?
         if t_start is None:
             t_start = min(self.time_points)
         if t_end is None:
@@ -42,16 +56,16 @@ class OdeFitter:
         if t_start is not None or t_end is not None:
             selected_time_points = (t_start <= t) & (t <= t_end)
             t = t[selected_time_points]
-            y_real = y_real[:, selected_time_points]
         y0 = [value for name, value in params.valuesdict().items()
               if name in self.init_condition_names]
         tuple_params = [value for name, value in params.valuesdict().items()
                         if name not in self.init_condition_names]
         # Seems to take extremely long sometimes
         y_pred = solve_ivp(my_func, (t_start, t_end), y0, t_eval=t,
-                           args=tuple_params, method='RK23', first_step=1)
-        assert y_pred.status >= 0, f"Integration failed {y_pred.message}"
-        return np.square(y_pred.y - y_real)
+                           args=tuple_params)
+        assert y_pred.status >= 0, (f"Integration failed: {y_pred.message}"
+                                    f"\nParams: {params}")
+        return y_pred
 
     def fit(self, t_start=None, t_end=None) -> MinimizerResult:
         output = minimize(self.loss_function,
@@ -61,5 +75,11 @@ class OdeFitter:
                                'my_func': self.odes,
                                't_start': t_start,
                                't_end': t_end})
+        # logging.info('Fitting complete. Updating params')
+        # self.params = output.params
+        # self.fitting_complete = True
         return output
         # output.params.pretty_print()
+
+    def set_params(self):
+        raise NotImplementedError
--- tests/test_ModuleRegulatoryNetwork.py
+++ tests/test_ModuleRegulatoryNetwork.py
@@ -3,6 +3,7 @@ import logging
 import numpy as np
 import pytest
 from lmfit import fit_report
+from matplotlib import pyplot as plt
 
 from ExpressionMatrix import ExpressionMatrixTimeSeries
 from ModuleRegulatoryNetwork import ModuleRegulatoryNetwork
@@ -25,12 +26,56 @@ def test_fit_ode_to_data(
     my_time_series_expressions.keep_only_shoot()
     my_time_series_expressions.merge_biological_samples()
     my_time_series_expressions.keep_only_de_genes(std_cutoff=1.5)
-    # TODO check if my_data is correct?
+    # TODO check if my_data is correct format and not inverted or something
     my_time, my_data = \
         my_time_series_expressions.get_clusters_expressions_with_time(n_clusters)
     fitter = OdeFitter(my_ode, my_data, my_time)
-    # Note: this is slower if parameters have negative values atm
+    # Note: look into the initial parameter values
     optimal_fit = fitter.fit()
-    # TODO compare this to actual data and incorporate inhibition mechanisms?
     logging.info(fit_report(optimal_fit))
     assert True
+
+
+def test_identifiability(
+        my_ode: OdeInference,
+        my_time_series_expressions: ExpressionMatrixTimeSeries
+):
+    """
+    Simulate some data, and then try to fit an ODE to it to test identifiability.
+    """
+    my_time = np.array([15., 30., 60., 180., 240., 360., 720., 1440.])
+    my_time = np.linspace(0, 1000, 1000)
+    generator = OdeFitter(my_ode, measured_data=np.random.rand(4, 1), time_points=my_time)
+    # generator.set_params({})
+    my_params = generator.params
+    print('Modify that shit now')
+    my_params['beta3'].value = 0.1
+    my_params['beta0'].value = my_params['beta1'].value = 0.05
+    my_params['beta2'].value = my_params['beta4'].value = -0.05
+    for i in [0, 1, 2, 4]:
+        my_params[f'd{i}'].value = 0.01
+    sim_data = generator.predict_values(my_params,
+                                        generator.time_points,
+                                        generator.odes)
+    # for row in sim_data.y:
+    #     plt.plot(sim_data.t, row)
+
+    fitter = OdeFitter(my_ode, sim_data.y, sim_data.t)
+    # Note: look into the initial parameter values
+    fit_result = fitter.fit()
+    # TODO compare this to actual data and incorporate inhibition mechanisms?
+    logging.info(fit_report(fit_result))
+    logging.info(f'{my_params=}')
+
+
+    optimal_fit = fitter.predict_values(fit_result.params, sim_data.t, my_ode)
+    colours = ['b', 'r', 'g', 'y']
+    for i in range(4):
+        colour = colours[i]
+        true = sim_data.y[i, :]
+        fit = optimal_fit.y[i, :]
+        plt.plot(my_time, true, '-', color=colour)
+        plt.plot(my_time, fit, '--', color=colour)
+    plt.show()
+    assert True
+
