4d2a7945ff654c8d2e43e1ce4db58eb6c555796a
noord087 <ben.noordijk@wur.nl>
2023-03-24 15:13:05 +0100

    Minor changes to tests

--- tests/test_OdeFitter.py
+++ tests/test_OdeFitter.py
@@ -1,6 +1,8 @@
+import logging
+
 import networkx as nx
 import numpy as np
-from lmfit import Parameters
+from lmfit import Parameters, fit_report
 
 from DynamicModels.ModuleRegulatoryNetwork import ModuleRegulatoryNetwork, \
     EdgeRelation
@@ -10,6 +12,8 @@ from helpers import plot_y_and_y_hat
 
 
 def test_artificial_data_fit():
+    """Generate artificial data from nonlinear model, and fit a new model to it.
+    """
     my_graph = nx.DiGraph()
     module_names = [ModuleRegulatoryNetwork.module_prefix + str(i)
                     for i in range(1, 4)]
@@ -28,26 +32,29 @@ def test_artificial_data_fit():
         articial_network,
         nonlinear=True)
     init_condition_names = []
-    my_params = Parameters()
+    ground_truth_params = Parameters()
     for param in artificial_model.get_param_names():
-        value = 0 if 'temp' in param else 1
-        my_params.add(name=param, value=value)
+        ground_truth_params.add(name=param, value=1)
+
+    ground_truth_params['heat_temp'].set(value=.7)
+    ground_truth_params['non_heat_temp'].set(value=.3)
+    ground_truth_params['heat_end_time'].set(value=10)
 
     # Add initial conditions
     for module_name in module_names:
         value = 5 if '1' in module_name else 0
-        init_name = f'{module_name}_start'
-        my_params.add(name=init_name, value=value)
+        init_name = f'y{int(module_name[-1])-1}'
+        ground_truth_params.add(name=init_name, value=value)
         init_condition_names.append(init_name)
-    sim_data = artificial_model.calculate_solution(
-        my_params, t=np.linspace(0, 24),
-        init_condition_names=init_condition_names)
+    sim_data = artificial_model.calculate_solution(ground_truth_params,
+                                                   t=np.linspace(0, 24),
+                                                   init_condition_names=init_condition_names)
 
-    plot_y_and_y_hat(sim_data.y, sim_data.t)
+    # plot_y_and_y_hat(sim_data.y, sim_data.t)
 
-    my_fitter = OdeFitter(artificial_model, sim_data.y, sim_data.t)
-    for param in my_fitter.params:
-        my_fitter.params[param].set(value=1.1)
+    my_fitter = OdeFitter(artificial_model, sim_data.y, sim_data.t, heat_end_time=10, param_limit=30)
+    # my_fitter.params['heat_temp'].set(value=0, vary=False)
+    # my_fitter.params['non_heat_temp'].set(value=0, vary=False)
     my_fitter.fit()
     fit_result = my_fitter.calculate_current_best_fit(sim_data.t)
     plot_y_and_y_hat(sim_data.y, sim_data.t, fit_result)
