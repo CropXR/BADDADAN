f0facc5ad612a5925a8dd655144aa1943cb553dd
noord087 <ben.noordijk@wur.nl>
2023-03-24 15:13:05 +0100

    Minor changes to tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 
 from Expressions.ExpressionMatrix import ExpressionMatrixTimeSeries
 from DynamicModels.OdeModel import OdeModel
 
-from main import fit_ode_to_data, thickening_thinning
+from main import fit_ode_to_data, thickening_thinning, \
+    annotate_microarray_expression
 
 
 def test_get_tf2_input(my_tf2_input):
@@ -24,10 +26,21 @@ def test_tf2_inference_with_extra_connection(
         my_tf2_ode: OdeModel,
         my_time_series_expressions: ExpressionMatrixTimeSeries):
     # TODO how do we know for sure if the modules did not get mixed up?
-    fit_ode_to_data(my_tf2_ode, my_time_series_expressions, std_cutoff=1)
+    """Do inference on original dataset with model that contains additional connections"""
+    my_tf2_ode.add_regulator_to_module(target_module_idx=1, origin_module_idx=3)
+    my_tf2_ode.add_regulator_to_module(target_module_idx=3, origin_module_idx=1)
+    fit_ode_to_data(my_tf2_ode, my_time_series_expressions)
+
+def test_tf2_inference_with_fewer_connection(
+        my_tf2_ode: OdeModel,
+        my_time_series_expressions: ExpressionMatrixTimeSeries):
+    """Do inference on original dataset with model that contains one fewer connection"""
+    my_tf2_ode.remove_regulator_from_module(target_module_idx=2,
+                                            origin_module_idx=1)
+    fit_ode_to_data(my_tf2_ode, my_time_series_expressions)
 
 def test_thickening_thinning(
-        my_tf2_ode,
+        my_tf2_ode: OdeModel,
         my_time_series_expressions: ExpressionMatrixTimeSeries
 ):
     thickening_thinning(my_tf2_ode, my_time_series_expressions, std_cutoff=1)
