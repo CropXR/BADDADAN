5105acf086bbd98737c5584796810cbdff68afa8
noord087 <ben.noordijk@wur.nl>
2023-03-27 16:08:08 +0200

    Multistart ODE fitting

--- DynamicModels/helper_scripts_for_fitting.py
+++ DynamicModels/helper_scripts_for_fitting.py
@@ -6,6 +6,13 @@ from DynamicModels.OdeFitter import OdeFitter
 
 
 def fit_multiple_fitters(fitters: list[OdeFitter]) -> OdeFitter:
+    """Fit a list of fitters simultaneously, and return the best fit
+
+    :param fitters: list of OdeFitter instances. Length of list determines
+     how many OdeFitters are run in parallel. E.g. 5 items in list means
+     5 paralllel processes.
+    :return: OdeFitter with best parameters
+    """
     caller = operator.methodcaller('fit')
     nr_starts = len(fitters)
     with Pool(nr_starts) as p:
@@ -14,4 +21,11 @@ def fit_multiple_fitters(fitters: list[OdeFitter]) -> OdeFitter:
     bic_values = [sol.bic for sol in all_fits]
     lowest_bic = min(bic_values)
     best_index = bic_values.index(lowest_bic)
+    assert all_fits[best_index].success, f'Best fit was failed fit :(' \
+                                         f'\n{all_fits[best_index].message}'
+    # Set the correct parameters based on the best fit; OdeFitter instances
+    # are not changed inplace.
+    best_fit = fitters[best_index]
+    best_fit.params = all_fits[best_index].params
+    best_fit.has_been_fitted = True
     return fitters[best_index]
--- tests/test_OdeFitter.py
+++ tests/test_OdeFitter.py
@@ -8,6 +8,7 @@ from DynamicModels.ModuleRegulatoryNetwork import ModuleRegulatoryNetwork, \
     EdgeRelation
 from DynamicModels.OdeFitter import OdeFitter
 from DynamicModels.OdeModel import OdeModel
+from DynamicModels.helper_scripts_for_fitting import fit_multiple_fitters
 from helpers import plot_y_and_y_hat
 
 
@@ -16,13 +17,19 @@ def test_artificial_data_fit():
     """
     my_graph = nx.DiGraph()
     module_names = [ModuleRegulatoryNetwork.module_prefix + str(i)
-                    for i in range(1, 4)]
+                    for i in range(1, 5)]
     my_graph.add_nodes_from(module_names)
     my_graph.add_edge(module_names[0], module_names[1],
-                      origin=EdgeRelation.UPREGULATES)
+                      origin=EdgeRelation.DOWNREGULATES)
     my_graph.add_edge(module_names[1], module_names[2],
                       origin=EdgeRelation.DOWNREGULATES)
+    my_graph.add_edge(module_names[2], module_names[1],
+                      origin=EdgeRelation.UPREGULATES)
     my_graph.add_edge(module_names[0], module_names[2],
+                      origin=EdgeRelation.UPREGULATES)
+    my_graph.add_edge(module_names[2], module_names[3],
+                      origin=EdgeRelation.DOWNREGULATES)
+    my_graph.add_edge(module_names[3], module_names[0],
                       origin=EdgeRelation.UPREGULATES)
 
     articial_network = ModuleRegulatoryNetwork(my_graph)
@@ -34,28 +41,35 @@ def test_artificial_data_fit():
     init_condition_names = []
     ground_truth_params = Parameters()
     for param in artificial_model.get_param_names():
+        # value = -1 if 'gamma' in param else 1
         ground_truth_params.add(name=param, value=1)
 
     ground_truth_params['heat_temp'].set(value=.7)
     ground_truth_params['non_heat_temp'].set(value=.3)
-    ground_truth_params['heat_end_time'].set(value=10)
+    ground_truth_params['heat_end_time'].set(value=3)
 
     # Add initial conditions
     for module_name in module_names:
-        value = 5 if '1' in module_name else 0
+        value = 2 if '1' in module_name else 1
         init_name = f'y{int(module_name[-1])-1}'
         ground_truth_params.add(name=init_name, value=value)
         init_condition_names.append(init_name)
+
+    time_points = np.array([.25, .5, 1., 3., 4., 6., 12., 24.])
+    # time_points = np.linspace(0, 24, 50)
     sim_data = artificial_model.calculate_solution(ground_truth_params,
-                                                   t=np.linspace(0, 24),
+                                                   t=time_points,
                                                    init_condition_names=init_condition_names)
 
-    # plot_y_and_y_hat(sim_data.y, sim_data.t)
+    plot_y_and_y_hat(sim_data.y, sim_data.t)
 
-    my_fitter = OdeFitter(artificial_model, sim_data.y, sim_data.t, heat_end_time=10, param_limit=30)
-    # my_fitter.params['heat_temp'].set(value=0, vary=False)
-    # my_fitter.params['non_heat_temp'].set(value=0, vary=False)
-    my_fitter.fit()
+    # Fit multiple models simultaneously
+    nr_fits = 5
+    fitters = [OdeFitter(artificial_model, sim_data.y, sim_data.t,
+                         heat_end_time=3, param_limit=10)
+               for _ in range(nr_fits)]
+    my_fitter = fit_multiple_fitters(fitters)
+
     fit_result = my_fitter.calculate_current_best_fit(sim_data.t)
     plot_y_and_y_hat(sim_data.y, sim_data.t, fit_result)
     return
