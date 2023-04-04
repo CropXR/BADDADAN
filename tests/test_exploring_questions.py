a2048358453acc1ae9a64af624b7b363291cb2df
noord087 <ben.noordijk@wur.nl>
2023-04-04 10:48:28 +0200

    Remove one connection from inferred graph, and increased arrow size in plot

--- tests/test_exploring_questions.py
+++ tests/test_exploring_questions.py
@@ -4,8 +4,8 @@ from exploring_questions import inference_with_thickened_ode_structure, \
     inference_with_thinned_ode_structure
 
 
-def test_inference_with_wrong_ode_structure(my_tf2_ode: OdeModel,
+def test_inference_with_wrong_ode_structure(my_tf2_network,
                                             my_time_series_expressions: ExpressionMatrixTimeSeries):
-    inference_with_thickened_ode_structure(my_tf2_ode)
-    inference_with_thinned_ode_structure(my_tf2_ode)
+    inference_with_thickened_ode_structure(my_tf2_network)
+    inference_with_thinned_ode_structure(my_tf2_network)
 
