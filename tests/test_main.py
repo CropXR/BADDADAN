7dd3a485b3b8025d4f512febac412bcebaa7d8aa
noord087 <ben.noordijk@wur.nl>
2023-04-19 10:50:08 +0200

    Comparing all the analyses

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from Expressions.ExpressionMatrix import ExpressionMatrixTimeSeries
@@ -40,6 +41,10 @@ def test_thickening_thinning(
     thickening_thinning(my_tf2_network, my_time_series_expressions, std_cutoff=1)
 
 def test_compare_clusterings():
-    log2_cluster = Path('../data/time_series_datasets/tf2network_approach/2000_highest_qcd_log2/02_gene_to_module.csv')
-    raw_cluster = Path('../data/time_series_datasets/tf2network_approach/2000_highest_qcd_no_log2/02_gene_to_module.csv')
-    compare_clusterings(log2_cluster, raw_cluster)
+    # log2_cluster = Path('../data/time_series_datasets/tf2network_approach/2000_highest_qcd_log2/01_tf2network_input_2000_highest_qcd_genes.txt')
+    # raw_cluster = Path('../data/time_series_datasets/tf2network_approach/2000_highest_qcd_no_log2/01_tf2network_input_2000_highest_qcd_genes.txt')
+    for method in ['mad', 'cv', 'qcd']:
+        logging.info(f'Method= {method}')
+        log2_cluster = Path(f'../data/time_series_datasets/tf2network_approach/2000_highest_{method}_log2/01_tf2network_input_2000_highest_{method}_genes.txt')
+        raw_cluster = Path(f'../data/time_series_datasets/tf2network_approach/2000_highest_{method}_no_log2/01_tf2network_input_2000_highest_{method}_genes.txt')
+        compare_clusterings(log2_cluster, raw_cluster)
