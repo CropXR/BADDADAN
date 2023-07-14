c16e09fc8a80ace933ec4b535bc5038a7ccab903
noord087 <ben.noordijk@wur.nl>
2023-07-14 10:42:16 +0200

    Provide full basic code snippet for camila

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ def fit_ode_to_two_datasets(
         module_network: ModuleRegulatoryNetwork,
         my_time_series_expressions: ExpressionMatrixTimeSeries,
         control_experiment: ExpressionMatrixTimeSeries = None):
-    # Assume that data has already been clustered
+    # Assure that data has already been clustered
     assert my_time_series_expressions.has_been_clustered
     assert control_experiment.has_been_clustered
     my_ode = OdeModel.construct_from_regulatory_network(module_network,
@@ -490,6 +490,89 @@ def count_flowering_genes(path_to_gene_selection: Path,
     overlap_df = selected_gene_df.merge(flowering_gene_df, left_on='gene',
                                         right_on='locustag')
     return overlap_df
+
+
+def camila_red_panda(soft_file_in_path: Path,
+                     out_dir: Path,
+                     do_log2: bool = True,
+                     variation_measure: str = 'mad',
+                     nr_total_genes: int = 2000,
+                     nr_clusters: int = 4):
+    """Minimum working example on how to do the full procedure of
+    parsing expression data, clustering it, and fitting model to it.
+
+    Currently I'm exposing quite a bit of code to you, but I think that
+    will help you fix things if you ever run into issues ;)
+    """
+    expression_matrix = ExpressionMatrixTimeSeries.from_geo_file(
+        soft_file_in_path, log2_transform=do_log2)
+
+    # # Uncomment if you want to use a second expressionmatrix
+    # # during clustering as well
+    # control_expr_mat_time = ExpressionMatrixTimeSeries.from_geo_file(
+    #     some_other_in_path, log2_transform=do_log2)
+    # expression_matrix.concat_to_expression_matrix(control_expr_mat_time,
+    #                                           keys=['Heat', 'Control'])
+
+    expression_matrix.keep_n_most_deviating_genes(nr_total_genes,
+                                                  variation_measure)
+    expression_matrix.do_hierachical_clustering(n_cluster=nr_clusters,
+                                                do_plotting=False)
+    # # Also uncomment if you want to use a second expressionmatrix
+    # # during clustering
+    # expression_matrix.remove_condition_from_expression_matrix('Control')
+    # control_expr_mat_time.assign_clusters_from(expr_mat_time)
+
+    # Create a file that can be used on http://bioinformatics.psb.ugent.be/webtools/TF2Network/
+    # to get putative regulators per cluster
+    expression_matrix.write_tf2_input_file(
+        out_dir / f'01_tf2network_input_{nr_total_genes}_highest_{variation_measure}_genes.txt',
+        omit_unannotated_genes=True)
+    # On TF2Network, make sure to select direct_export
+    expression_matrix.save_tf_produced_by_module_file(
+        out_dir / f'02_gene_to_module.csv',
+        tf_list_path=Path('data/resources/Ath_TF_list.txt')
+    )
+
+    # From TF2 output, create gene regulatory network
+    my_grn = ModuleRegulatoryNetwork.from_tf2_tsv(
+        out_dir / '03_tf2network_output.tsv')
+    my_grn.add_tf_module_mappings(out_dir / '02_gene_to_module.csv')
+    my_grn.clean_up_network()
+    # my_grn.plot_network(with_labels=True)
+    my_grn.check_if_tfs_created_by_module(expression_matrix, do_plotting=False,
+                                          remove_low_corr=True)
+    my_grn.set_up_or_downregulation(expression_matrix, do_plotting=False)
+    module_module = my_grn.get_module_module_network()
+    module_module.plot_network(with_labels=True)
+
+    # Assume that data has already been clustered
+    assert expression_matrix.has_been_clustered
+    my_time, my_data = expression_matrix.get_clusters_expressions_with_time(
+        0, aggregation_method='mean')
+    # my_time_series_expressions.plot_clusters_over_time(plot_units=True)
+
+    # Create system of ordinary differential equations
+    my_ode = OdeModel.construct_from_regulatory_network(module_module,
+                                                        nonlinear=True)
+    logging.info(my_ode)
+
+    # Fit using gradient descent with multiple starting
+    nr_fits = 5
+    fitters = [OdeFitter(my_ode, my_data, my_time,
+                         heat_end_time=3, param_limit=100)
+               for _ in range(nr_fits)]
+    best_fit = fit_multiple_fitters(fitters, nr_iters=1000, extra_analysis=False)
+    best_fit.params.pretty_print()
+    logging.info(f'Best fit parameters {best_fit.params.valuesdict()}')
+
+    # See how best fit looks compared to the experimental data
+    simulated_data = best_fit.calculate_current_best_fit(my_time)
+    # best_fit.plot_hill_equation_range(my_time)
+
+    plot_y_and_y_hat(y_real=my_data, t_real=my_time,
+                     model_fit=simulated_data)
+    plt.show()
 
 
 if __name__ == "__main__":
