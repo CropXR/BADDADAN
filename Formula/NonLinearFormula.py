bb62826b622357c619c5386eb72b59e3188087eb
noord087 <ben.noordijk@wur.nl>
2024-01-17 13:03:07 +0100

    Hill equation first order again

--- Formula/NonLinearFormula.py
+++ Formula/NonLinearFormula.py
@@ -226,10 +226,10 @@ class NonLinearFormula(FormulaSuperClass):
         self.params.extend([b_param_name, k_param_name])
         if direction == EdgeRelation.UPREGULATES:
             term_to_add = self.generate_hill_activation_term(
-                b_param_name, k_param_name, var_name, n=10)
+                b_param_name, k_param_name, var_name, n=1)
         elif direction == EdgeRelation.DOWNREGULATES:
             term_to_add = self.generate_hill_inhibition_term(
-                b_param_name, k_param_name, var_name, n=10)
+                b_param_name, k_param_name, var_name, n=1)
         else:
             raise NotImplementedError('If regulatory interaction is unclear, cannot use that properly yet')
         self.formula_parts.append(term_to_add)
